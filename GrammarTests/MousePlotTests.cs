b5bbb00d115671230e3f84f9fba6ceec54bf7384
mrworkman <workman.stephen@gmail.com>
2017-12-29 01:17:13 -0500

    Add tests for secondary screens.
    Also, when coordinates are outside the bounds of the screen, make sure the mouse pointer is actually not moved ourside.

--- GrammarTests/MousePlotTests.cs
+++ GrammarTests/MousePlotTests.cs
@@ -63,11 +63,11 @@ namespace GrammarTests {
       }
 
       [Test]
-      [TestCase("10", 0)]
-      [TestCase("99", 0)]
-      [TestCase("a", 0)]
+      [TestCase("10",    0)]
+      [TestCase("99",    0)]
+      [TestCase("a",     0)]
       [TestCase("alpha", 0)]
-      [TestCase("Bob", 0)]
+      [TestCase("Bob",   0)]
       public void InvalidCoordinateShouldThrowException(String c, Int32 expected) {
          Assert.That(
             () => _grammar.GetCoordinateOrdinal(c),
@@ -82,9 +82,9 @@ namespace GrammarTests {
       [TestCase(7, 750)]
       [TestCase(12, 1250)]
       [TestCase(15, 1550)]
-      [TestCase(19, 1920)] // <-- Beyond the edge of the screen
-      [TestCase(20, 1920)] // <-- Beyond the edge of the screen
-      [TestCase(36, 1920)] // <-- Beyond the edge of the screen
+      [TestCase(19, 1919)] // <-- Beyond the edge of the screen
+      [TestCase(20, 1919)] // <-- Beyond the edge of the screen
+      [TestCase(36, 1919)] // <-- Beyond the edge of the screen
       public void ShouldGetCorrectXCoordFor100X100CellsOn1920X1080Screen(Int32 x, Int32 expected) {
          _screenMock.Setup(e => e.Bounds).Returns(
             new Rectangle(0, 0, 1920, 1080)
@@ -99,8 +99,8 @@ namespace GrammarTests {
       [TestCase(2,  250 )]
       [TestCase(7,  750 )]
       [TestCase(10, 1050)]
-      [TestCase(11, 1080)] // <-- Beyond the edge of the screen
-      [TestCase(36, 1080)] // <-- Beyond the edge of the screen
+      [TestCase(11, 1079)] // <-- Beyond the edge of the screen
+      [TestCase(36, 1079)] // <-- Beyond the edge of the screen
       public void ShouldGetCorrectYCoord100X100CellsOn1920X1080Screen(Int32 y, Int32 expected) {
          _screenMock.Setup(e => e.Bounds).Returns(
             new Rectangle(0, 0, 1920, 1080)
@@ -115,8 +115,8 @@ namespace GrammarTests {
       [TestCase(2, 250)]
       [TestCase(7, 750)]
       [TestCase(9, 950)]
-      [TestCase(10, 1024)] // <-- Beyond the edge of the screen
-      [TestCase(15, 1024)] // <-- Beyond the edge of the screen
+      [TestCase(10, 1023)] // <-- Beyond the edge of the screen
+      [TestCase(15, 1023)] // <-- Beyond the edge of the screen
       public void ShouldGetCorrectXCoordFor100X100CellsOn1024X768Screen(Int32 x, Int32 expected) {
          _screenMock.Setup(e => e.Bounds).Returns(
             new Rectangle(0, 0, 1024, 768)
@@ -130,9 +130,9 @@ namespace GrammarTests {
       [TestCase(1,  150)]
       [TestCase(2,  250)]
       [TestCase(7,  750)]
-      [TestCase(8,  768)] // <-- Beyond the edge of the screen
-      [TestCase(9,  768)] // <-- Beyond the edge of the screen
-      [TestCase(36, 768)] // <-- Beyond the edge of the screen
+      [TestCase(8,  767)] // <-- Beyond the edge of the screen
+      [TestCase(9,  767)] // <-- Beyond the edge of the screen
+      [TestCase(36, 767)] // <-- Beyond the edge of the screen
       public void ShouldGetCorrectYCoord100X100CellsOn1024X768Screen(Int32 y, Int32 expected) {
          _screenMock.Setup(e => e.Bounds).Returns(
             new Rectangle(0, 0, 1024, 768)
@@ -140,5 +140,60 @@ namespace GrammarTests {
 
          Assert.That(_grammar.GetMouseYCoord(y), Is.EqualTo(expected));
       }
+
+      [Test]
+      [TestCase(0,  50 - 1920)]
+      [TestCase(1,  150 - 1920)]
+      [TestCase(2,  250 - 1920)]
+      [TestCase(7,  750 - 1920)]
+      [TestCase(12, 1250 - 1920)]
+      [TestCase(15, 1550 - 1920)]
+      [TestCase(19, 1919 - 1920)] // <-- Beyond the edge of the screen
+      [TestCase(20, 1919 - 1920)] // <-- Beyond the edge of the screen
+      [TestCase(36, 1919 - 1920)] // <-- Beyond the edge of the screen
+      public void ShouldGetCorrectXCoordFor100X100CellsOn1920X1080SecondaryScreen(Int32 x, Int32 expected) {
+
+         // Simulate a secondary screen that's to the left of the primary screen
+         _screenMock.Setup(e => e.Bounds).Returns(
+            new Rectangle(-1920, 0, 1920, 1080)
+         );
+
+         Assert.That(_grammar.GetMouseXCoord(x), Is.EqualTo(expected));
+      }
+
+      [Test]
+      [TestCase(0,  50 - 1024)]
+      [TestCase(1,  150 - 1024)]
+      [TestCase(2,  250 - 1024)]
+      [TestCase(7,  750 - 1024)]
+      [TestCase(9,  950 - 1024)]
+      [TestCase(10, 1023 - 1024)] // <-- Beyond the edge of the screen
+      [TestCase(11, 1023 - 1024)] // <-- Beyond the edge of the screen
+      public void ShouldGetCorrectYCoord100X100CellsOn1920X1080SecondaryScreen(Int32 y, Int32 expected) {
+         // Simulate a secondary screen that's above the primary screen
+         _screenMock.Setup(e => e.Bounds).Returns(
+            new Rectangle(0, -1024, 1280, 1024)
+         );
+
+         Assert.That(_grammar.GetMouseYCoord(y), Is.EqualTo(expected));
+      }
+
+      [Test]
+      [TestCase(0, 50 + 1080)]
+      [TestCase(1, 150 + 1080)]
+      [TestCase(2, 250 + 1080)]
+      [TestCase(7, 750 + 1080)]
+      [TestCase(9, 950 + 1080)]
+      [TestCase(10, 1023 + 1080)] // <-- Beyond the edge of the screen
+      [TestCase(11, 1023 + 1080)] // <-- Beyond the edge of the screen
+      public void ShouldGetCorrectYCoord100X100CellsOn1920X1080SecondaryScreenBelow(Int32 y, Int32 expected) {
+         // Simulate a secondary screen that's below a larger primary screen
+         _screenMock.Setup(e => e.Bounds).Returns(
+            new Rectangle(0, 1080, 1280, 1024)
+         );
+
+         Assert.That(_grammar.GetMouseYCoord(y), Is.EqualTo(expected));
+      }
+
    }
 }
