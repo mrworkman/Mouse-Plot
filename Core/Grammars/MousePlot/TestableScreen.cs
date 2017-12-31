9a366e0a7f18e230e1468dc08aaa9ad0e461d79e
mrworkman <workman.stephen@gmail.com>
2017-12-31 12:24:10 -0500

    Update application manifest to improve DPI scaling in Windows 10 1607+.

--- Core/Grammars/MousePlot/TestableScreen.cs
+++ Core/Grammars/MousePlot/TestableScreen.cs
@@ -15,8 +15,10 @@
 // along with this program.If not, see<http://www.gnu.org/licenses/>.
 //
 
+using System;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace Renfrew.Core.Grammars.MousePlot {
